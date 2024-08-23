989e177bd72c0214daa67786a439edddab5ffb5e
Otavio Cardoso <otavio.cardoso@rfn.ptc>
2024-08-23 17:55:16 -0300

    fix: alteração classe de conexão com o banco de dados

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,16 +2,15 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 async function main() {
-  const mongoURI = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`;
+  // const mongoURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
+  const mongoURI = `mongodb+srv://user_admin:KIxlJhA81MPZcCzw@cluster0.uztjm.mongodb.net/freelancer_hub?retryWrites=true&w=majority`;
 
   try {
-    await mongoose.connect(
-      mongoURI
-    );
+    await mongoose.connect(mongoURI);
 
     console.log('Connection to mongo successful!');
-  } catch (err) {
-    console.log(err.message);
+  } catch (error) {
+    console.log(error.message);
   }
 };
 
